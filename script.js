d24b2b9739f7bf8cdea92bd9bd60c0a7bc5a2217
charoiteersarathi-hash <charoiteersarathi@gmail.com>
2026-07-02 12:42:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
-body{
-    margin:0;
-    background:linear-gradient(135deg,#ff4d6d,#ff8fab);
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    height:100vh;
-    font-family:Arial,sans-serif;
-    overflow:hidden;
-}
+function openGift(){
 
-.container{
-    text-align:center;
-    color:white;
-}
+    let gift=document.querySelector(".gift-box");
+    let msg=document.getElementById("message");
 
-h1{
-    font-size:32px;
-    margin-bottom:30px;
-}
+    gift.innerHTML="💖";
+    msg.innerHTML="Happy Birthday Tani Shannon ❤️";
 
-.gift-box{
-    font-size:120px;
-    cursor:pointer;
-    animation:shake 1s infinite;
-}
-
-#message{
-    margin-top:20px;
-    font-size:22px;
-}
-
-@keyframes shake{
-    0%{transform:rotate(0deg);}
-    25%{transform:rotate(-10deg);}
-    50%{transform:rotate(10deg);}
-    75%{transform:rotate(-10deg);}
-    100%{transform:rotate(0deg);}
-}
-
-.opened{
-    animation:none;
-    transform:scale(1.3);
 }
