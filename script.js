c45f8bc99bb4f41556d4e5c0ba5550b5ca4d03ba
charoiteersarathi-hash <charoiteersarathi@gmail.com>
2026-07-02 12:57:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+const photos = [
+    "photos/photo1.jpg",
+    "photos/photo2.jpg",
+    "photos/photo3.jpg",
+    "photos/photo4.jpg",
+    "photos/photo5.jpg"
+];
+
+let current = 0;
+
 function openGift(){
 
-    let gift=document.querySelector(".gift-box");
-    let msg=document.getElementById("message");
+    document.querySelector(".gift-box").innerHTML="💖";
 
-    gift.innerHTML="💖";
-    msg.innerHTML="Happy Birthday Tani Shannon ❤️";
+    document.getElementById("message").innerHTML =
+    "Happy Birthday Tani Shannon ❤️";
+
+    document.getElementById("slideshow").style.display = "block";
+
+    setInterval(() => {
+
+        current++;
+
+        if(current >= photos.length){
+            current = 0;
+        }
+
+        document.getElementById("slide").src =
+        photos[current];
+
+    },3000);
 
 }
